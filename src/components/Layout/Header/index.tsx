e961ca0ee36c30db9e93b0b2a8d59d8993ad6535
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2024-08-10 12:30:48 +0200

    style

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -4,13 +4,13 @@ import logo from "../../../assets/Logo/logo2.png";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 z-99 w-full bg-white shadow-lg h-18 ">
-      <div className=" px-4 md:px-8 flex flex-row justify-between items-center">
+    <header className="fixed top-0 left-0 z-99 w-full bg-white shadow-md h-18 ">
+      <div className=" py-2 px-4 md:px-8 flex flex-row justify-between items-center">
         <Link to="/" className="flex flex-row justify-center items-center">
           <img
             src={logo}
             alt="The logo of Issas"
-            style={{ width: "150px", height: "100px" }}
+            style={{ width: "200px", height: "80px" }}
           />
         </Link>
         <div className="flex flex-row justify-end ">
