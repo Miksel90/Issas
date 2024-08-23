0784314a6b52e3048f000de461db26e53e113a71
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2024-08-23 15:43:41 +0200

    contatInfo

--- src/pages/ContactPeople/contactData.ts
+++ src/pages/ContactPeople/contactData.ts
@@ -15,15 +15,15 @@ export const people: Person[] = [
   {
     name: "Roald Selstad",
     title: "Daglig leder",
-    phone: "+47 669 63 500",
-    email: "post@issas.no",
+    phone: "+47 99204383",
+    email: "roald@issas.no",
     image: image1,
   },
   {
     name: "Jonas Selstad",
     title: "Prosjekt leder",
-    phone: "+47 669 63 500",
-    email: "post@issas.no",
+    phone: "+47 97132023",
+    email: "jonas@issas.no",
     image: image2,
   },
   {
